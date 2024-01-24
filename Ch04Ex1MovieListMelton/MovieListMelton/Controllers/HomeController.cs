207fb64f6e9cb2ba4f421d16ce282806a25b0dc4
KM <87396239+KyleMel@users.noreply.github.com>
2024-01-24 06:19:51 -0600

    Ch04MovieListMelton

---
+++ Ch04Ex1MovieListMelton/MovieListMelton/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieListMelton.Models;
+using System.Diagnostics;
+using System.Linq;
+
+namespace MovieListMelton.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies.OrderBy(m => m.Name).ToList();
+            return View(movies);
+        }
+    }
+}
